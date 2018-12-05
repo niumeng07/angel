b250567e4131d5c9d047b69530a43ad3f41c5168
xiaoyi.zhang <xiaoyi.zhang@mobvista.com>
2018-12-05 17:25:04 +0800

    test

--- source.sh
+++ source.sh
@@ -1,2 +1,9 @@
 PATH=/home/mobdev/liuda/protobuf-2.5.0/bin/:$PATH
-mvn install -Dmaven.test.skip=true
+mvn install -Dmaven.test.skip=true -DskipTests
+
+
+rm -rf ~/liuda/angel/angel/dist/target/temp/*
+cp ~/liuda/angel/angel/dist/target/angel-2.0.0-bin.zip ~/liuda/angel/angel/dist/target/temp/
+cd ~/liuda/angel/angel/dist/target/temp/
+unzip angel-2.0.0-bin.zip
+cp -r lib ~/liuda/angel/angel/dist/target/angel-2.0.0-bin/
